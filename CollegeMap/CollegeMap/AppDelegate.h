4fa77a88bb84251f44f7e683213579fb0e108c3f
Andrew <andrew@Shine-deMac-mini.local>
2015-11-02 11:44:44 +0800

    init

---
+++ CollegeMap/CollegeMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollegeMap
+//
+//  Created by Andrew on 15/11/2.
+//  Copyright (c) 2015年 Tecomtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollegeMap/CollegeMapTests/CollegeMapTests.m
@@ -0,0 +1,40 @@
+//
+//  CollegeMapTests.m
+//  CollegeMapTests
+//
+//  Created by Andrew on 15/11/2.
+//  Copyright (c) 2015年 Tecomtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CollegeMapTests : XCTestCase
+
+@end
+
+@implementation CollegeMapTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
