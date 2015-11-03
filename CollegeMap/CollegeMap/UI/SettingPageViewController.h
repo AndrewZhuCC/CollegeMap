c64957b38b8d14b052bf4186f1dc5a8b2cb8e471
Andrew <andrew@Shine-deMac-mini.local>
2015-11-03 13:19:22 +0800

    update for first setting cocoapods

---
+++ CollegeMap/CollegeMap/UI/SettingPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingPageViewController.h
+//  CollegeMap
+//
+//  Created by Andrew on 15/11/3.
+//  Copyright (c) 2015年 Tecomtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingPageViewController : UIViewController
+
+@end
---
+++ CollegeMap/Pods/Target Support Files/Pods-CollegeMap/Pods-CollegeMap-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CollegeMap : NSObject
+@end
+@implementation PodsDummy_Pods_CollegeMap
+@end
